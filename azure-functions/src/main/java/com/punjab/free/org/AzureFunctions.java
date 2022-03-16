a61b6dd8351e8ad5edb8eb62ced5480a67138df0
Ish Mahajan <ishmahajanbhanu@gmail.com>
2022-03-16 22:18:43 +0530

    initial commit

---
+++ azure-functions/src/main/java/com/punjab/free/org/AzureFunctions.java
@@ -0,0 +1,12 @@
+package com.punjab.free.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFunctions {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureFunctions.class, args);
+    }
+}
